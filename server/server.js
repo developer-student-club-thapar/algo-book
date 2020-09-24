8df130a78b6225cc0c14ddf0b387a73c5e2a7361
Rishabh Jain <rjain_be19@thapar.edu>
2020-09-24 08:17:12 +0530

    added db

--- server/server.js
+++ server/server.js
@@ -12,11 +12,9 @@ import db from "./config/db";
 
 process.on("uncaughtException", (err) => {
     console.log(err);
-    process.exit(1);
 });
 process.on("unhandledRejection", (err) => {
     console.log(err);
-    process.exit(2);
 });
 
 const app = express();
