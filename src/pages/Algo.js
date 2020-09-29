bf3036bae5d71daa7ff5bd886021a837ca536bab
Aryaman1706 <agrover1_be19@thapar.edu>
2020-09-30 03:52:24 +0530

    tabs

--- src/pages/Algo.js
+++ src/pages/Algo.js
@@ -1,27 +1,107 @@
 /*
-Page where 2 "folders" of Algo and DS would be displayed
+Tab where 2 "folders" of Algo and DS would be displayed
 
 Home
 */
 
-import React, { Fragment } from "react";
-import Folder from "../components/Folder";
+import React, { Fragment, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import {
+    Container,
+    AppBar,
+    Tab,
+    Tabs,
+    Paper,
+    makeStyles,
+} from "@material-ui/core";
 import BreadCrumbs from "../components/BreadCrumbs";
+import Folder from "../components/Folder";
 
-export default function Algo() {
+const Algo = () => {
+    const algo = ["Searching", "Sorting", "Traversal"];
+    const ds = ["Linked List", "Stacks", "Queues", "Graphs", "Trees"];
+
+    const AlgoComponent = () => {
+        return (
+            <Fragment>
+                {algo.map((x, index) => (
+                    <Folder name={x} link={`/algo/${x}`} key={index} />
+                ))}
+            </Fragment>
+        );
+    };
+
+    const DsComponent = () => {
+        return (
+            <Fragment>
+                {ds.map((x, index) => (
+                    <Folder name={x} link={`/ds/${x}`} key={index} />
+                ))}
+            </Fragment>
+        );
+    };
+
+    const { page } = useParams();
+
+    const pageToValue = {
+        algo: 0,
+        ds: 1,
+    };
+
+    const valueToPage = {
+        0: "algo",
+        1: "ds",
+    };
+
+    const [tab, setTab] = useState(pageToValue[page]);
+    const history = useHistory();
+
+    const onChange = (e, newtab) => {
+        e.preventDefault();
+        history.push(`/${valueToPage[newtab]}`);
+        setTab(newtab);
+    };
+
+    const renderPage = () => {
+        switch (page) {
+            case "algo":
+                return <AlgoComponent />;
+            /*{Render algo}*/
+
+            case "ds":
+                return <DsComponent />;
+            /*{Render ds}*/
+
+            default:
+                return <AlgoComponent />;
+            /*{Render algo}*/
+        }
+    };
     return (
         <Fragment>
-            <BreadCrumbs
-                crumbs={[
-                    { name: "Home", link: "/" },
-                    { name: "Algorithms", link: "/algo" },
-                    { name: "Sorting", link: "/algo/sorting" },
-                ]}
-                active="Active Link"
-            />
-            <br />
-            <Folder name="Algorithms" link="algo" />
-            <Folder name="Data Structures" link="ds" />
+            <Container fixed>
+                <BreadCrumbs
+                    crumbs={[{ name: "Home", link: "/" }]}
+                    active={valueToPage[tab]}
+                />
+                <Paper elevation={3} variant="outlined">
+                    <AppBar position="static" color="inherit">
+                        <Tabs
+                            value={tab}
+                            onChange={onChange}
+                            indicatorColor="primary"
+                            textColor="primary"
+                            variant="scrollable"
+                        >
+                            <Tab label="Algorithms" />
+                            <Tab label="Data Structures" />
+                        </Tabs>
+                    </AppBar>
+                </Paper>
+                {renderPage()}
+            </Container>
         </Fragment>
     );
-}
+};
+
+export default Algo;
