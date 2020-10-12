e628b5a6afd2cdd06b435ec1581f625f42715ecf
Atharva Kadlag <atharvakadlag@gmail.com>
2020-10-12 16:20:58 +0530

    Update palindrome.py

--- algorithms/Palindrome/palindrome.py
+++ algorithms/Palindrome/palindrome.py
@@ -7,26 +7,7 @@
 #  True: number is a palindrome
 #  False: number is a palindrome
 def palindrome(number): 
-    
-    # buffer to store the originalNumber
-    originalNumber = number
-
-    # computes the reverse of the number and stores it in reversedNumber
-    reversedNumber = 0
-    while number>0:
-        # calculates the last digit everytime
-        digit = number%10
-        # reversedNumber gets amplified by 10 & digit gets append to the one's position
-        reversedNumber *= 10
-        reversedNumber += digit
-        # interger division by 10 is applied to remove the added digit
-        number //= 10
-    
-    # checks if the numbers are equal.
-    if originalNumber == reversedNumber:
-        return True
-    else:
-        return False
+    return str(number)==str(number)[::-1] # keeping it simple
 
 # driver code to test above 
 if __name__ == "__main__":
